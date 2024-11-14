f6b42e899f6a0c206ac1049c724c0194dd76557b
ateki <irene.atek@btinternet.com>
2024-11-14 10:37:39 +0000

    feat: added data class for PlateauSize and Position

---
+++ src/main/java/com/northcoders/PlateauSize.java
@@ -0,0 +1,9 @@
+package com.northcoders;
+
+/** Currently plateau is of rectangle shape.
+ * TODO: consider if plateau were to be other 2d, 3d or multiD shapes
+ * @param length
+ * @param width
+ */
+public record PlateauSize(int length, int width) {
+}
