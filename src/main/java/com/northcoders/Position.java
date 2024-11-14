9aa04d18f557d2d68f7c4ba454eed691cb86c21a
ateki <irene.atek@btinternet.com>
2024-11-14 10:38:23 +0000

    added Position class prepresented by x,y coords and direction facing

---
+++ src/main/java/com/northcoders/Position.java
@@ -0,0 +1,11 @@
+package com.northcoders;
+
+/**
+ * Position represented by
+ *  x,y coordinates as well as direction facing.
+ */
+public class Position {
+    private int x;
+    private int y;
+    private CompassDirection facing;
+}
